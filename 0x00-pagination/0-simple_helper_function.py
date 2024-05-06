c820416beef48fb1026d5191354c82ae10602efe
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-06 19:53:14 +0100

    return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 """
+The function returns a tuple of size two containing s start index
+and an end index corresponding to the range of indexes to return
+in a list for those particular pagination parameters
+
+Page numbers are 1-indexed, i.e. the first page is 1
 """
 
 
 def index_range(page, page_size):
     """
+    Arguments:
+    - page and
+    - page_size
+
+    The function returns a tuple of size two containing s start index
+    and an end index corresponding to the range of indexes to return
+    in a list for those particular pagination parameters
+
+    Page numbers are 1-indexed, i.e. the first page is 1
     """
-    pass
+    return ((page_size * (page - 1)), (page_size * page))
