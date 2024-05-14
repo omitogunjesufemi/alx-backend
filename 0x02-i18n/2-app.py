d11e462bc3f312fa4073bcdf308ae6489ad44382
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-14 14:08:54 +0100

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Basic Flask App
+
+Create a single / route and an index.html template that simply
+outputs Welcome to Holberton as page title (<title>) and
+Hello world as header (<h1>).
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config())
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Welcome page"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
