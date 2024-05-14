996269a2227dbbcb4c3ded8d72d1168614c819ce
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-14 13:59:49 +0100

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,15 +7,27 @@ outputs Welcome to Holberton as page title (<title>) and
 Hello world as header (<h1>).
 """
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config())
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """Welcome page"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
