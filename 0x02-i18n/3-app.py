881ea9e71c3569856cbd82a1ad4d09d4c8634c10
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-15 05:28:13 +0100

    Fixed pycodestyle error

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -24,12 +24,14 @@ babel = Babel(app)
 app.config.from_object(Config())
 
 
-#@babel.localeselector
+@babel.localeselector
 def get_locale() -> str:
     """Get locale from request """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/', strict_slashes=False)
 def index() -> None:
     """Welcome page"""
