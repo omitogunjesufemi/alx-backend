c06eceeed4a77daadb7fb8905d0280306b6bd25d
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-15 09:36:04 +0100

    Infer appropriate timezone

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Basic Flask App
+
+Create a single / route and an index.html template that simply
+outputs Welcome to Holberton as page title (<title>) and
+Hello world as header (<h1>).
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+from typing import Dict
+import pytz
+
+
+class Config:
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request """
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
+        return locale
+
+    user_locale = g.user.get("locale")
+    if user_locale in app.config["LANGUAGES"]:
+        return user_locale
+
+    h_locale = request.headers.get("Accept-Language")
+    if h_locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """
+    Before returning a URL-provided or user time zone,
+    you must validate that it is a valid time zone. To that, use
+    pytz.timezone and catch the
+    pytz.exceptions.UnknownTimeZoneError exception.
+    """
+    time_zone = request.args.get('timezone')
+    if time_zone is app.config["BABEL_DEFAULT_TIMEZONE"]:
+        try:
+            return pytz.timezone(time_zone)
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    time_zone = g.user.get('timezone')
+    if time_zone is app.config["BABEL_DEFAULT_TIMEZONE"]:
+        try:
+            return pytz.timezone(time_zone)
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    return app.config["BABEL_DEFAULT_TIMEZONE"]
+
+
+def get_user() -> Dict:
+    """
+    Returns a user dictionary or None if the ID cannot be found
+    or if login_as was not passed
+    """
+    try:
+        user_id = int(request.args.get('login_as'))
+        return users.get(user_id)
+    except (TypeError, ValueError):
+        return None
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    Use get_user to find a user if any, and set it as a global
+    on flask.g.user
+    """
+    user = get_user()
+    g.user = user
+
+
+@app.route('/', strict_slashes=False)
+def index() -> None:
+    """Welcome page"""
+    return render_template("6-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
